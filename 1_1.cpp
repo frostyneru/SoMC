316330f4e56a530c8afb12a8b364bd948dad5c73
frostyneru <neru@protonmail.com>
2017-03-11 20:45:57 +0700

    Create 1_1.cpp

---
+++ 1_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float x,y,z,s;
+     int i;
+	cout << "x = ";
+	cin >> x;
+	cout << "y = ";
+	cin >> y;
+	cout << "z = ";
+	cin >> z;
+     cout << "x = " << x << " y = " << y << " z = " << z << "\v";
+	s=(x+y+z)*(x+y+z);
+	cout << "Answer : \v";
+     cout << "S = " << s << "\v";
+     for (i=0; i<12; i++)
+          cout << "\b";
+     cout << "********************" << endl;
+	return 0;
+}
