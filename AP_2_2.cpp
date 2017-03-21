3b5f93f9a0f40c051024c118c1720f914c0f19fd
frostyneru <neru@protonmail.com>
2017-03-21 18:07:18 +0700

    Create AP_2_2.cpp

---
+++ AP_2_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,i,s;
+	s=0;
+	do {
+		cout << "a = ";
+		cin >> a;
+		i++;
+		if (i % 2 != 0) {
+			s=s+a;
+		}
+	} while (a!=0);
+	cout << "Сумма нечетных элементов последовательности = " << s << endl;
+	return 0;
+}
