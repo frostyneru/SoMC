eba0dd482aa76bb6959ef7d0c20906d9d05432c7
frostyneru <neru@protonmail.com>
2017-03-21 16:28:03 +0700

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -4,34 +4,30 @@ using namespace std;
 int main()
 {
 	float a;
-     
 	int i = 0,j = 0;
-     cout << "Введите количество чисел : ";
-     cin >> j;
-     
-     float *d = new float [j];
+     	cout << "Введите количество чисел : ";
+     	cin >> j;
+     	float *d = new float [j];
 	ofstream fout("F.txt", ios_base::trunc);
-
 	while (i < j) 
 	{
-          cin >> d[i];
+         	cin >> d[i];
 		fout << d[i] << endl;
-          i+=1;
+          	i+=1;
 	}	
 	fout.close();
-
 	ofstream out("G.txt", ios_base::trunc);
 	ifstream fin("F.txt", ios_base::in);
-     a=d[0];
-     d[0]=d[j-1];
-     d[j-1]=a;
-     i=0;
+     	a=d[0];
+     	d[0]=d[j-1];
+     	d[j-1]=a;
+     	i=0;
 	while (i < j)
 	{
 		out << d[i] << endl;
-          i+=1;
+          	i+=1;
 	} 
-
 	fin.close();
 	out.close();
+	return 0;
 }
