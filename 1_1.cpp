0beab82ba807f1cac69e04c6b62d19270f0f7891
frostyneru <neru@protonmail.com>
2017-03-21 16:03:49 +0700

    Update 1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,20 +1,22 @@
+
 #include <iostream>
 using namespace std;
 int main()
 {
 	float x,y,z,s;
-     int i;
+     	int i;
 	cout << "x = ";
 	cin >> x;
 	cout << "y = ";
 	cin >> y;
 	cout << "z = ";
 	cin >> z;
-     cout << "x = " << x << " y = " << y << " z = " << z << "\v";
+     	cout << "x = " << x << " y = " << y << " z = " << z << "\v";
 	s=(x+y+z)*(x+y+z);
 	cout << "Answer : \v";
-     cout << "S = " << s << "\v";
-     for (i=0; i<12; i++)
-          cout << "\b";
-     cout << "********************" << endl;
+     	cout << "S = " << s << "\v";
+     	for (i=0; i<12; i++)
+        	cout << "\b";
+     	cout << "********************" << endl;
+	return 0;
 }
