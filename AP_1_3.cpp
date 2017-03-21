3b99287129ef89a5a8a4294318135611d97c9f8c
frostyneru <neru@protonmail.com>
2017-03-21 17:42:41 +0700

    Create AP_1_3.cpp

---
+++ AP_1_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+	cout << "x = ";
+	cin >> x;
+	cout << "y = ";
+	cin >> y;
+	if ((x*x+y*y) <=4)
+		cout << true << endl;
+	else
+		cout << false << endl;
+  return 0;
+}
