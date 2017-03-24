a9979769cff9b1539e58d18e2c98c5d45e1ed3c6
frostyneru <neru@protonmail.com>
2017-03-25 00:34:58 +0700

    Create 5_defence.cpp

---
+++ 5_defence.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <cstring>
+#include <sys/stat.h>
+using namespace std;
+int main()
+{
+	int result = mkdir("/home/ivan/Рабочий стол/folder/",0777);
+	int n=10;
+	char ch [10];
+	for (int i=0; i<n; i++)
+	{
+
+		char str[50]="/home/ivan/Рабочий стол/folder/";
+		sprintf(ch, "%d", i+1);
+		strcat(str,ch);
+		strcat(str,".txt");
+
+		fstream file(str, ios::out | ios::app);
+		file << n << endl;
+		file.close();
+	}
+	return 0;
+}
